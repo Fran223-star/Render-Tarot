a5e702535c0ec9583ffc516d6d109240a838b705
Fran223-star <senderodelaluna25@gmail.com>
2025-06-15 23:29:08 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://senderodelaluna.com.ar'],
+  origin: ['https://senderodelaluna.com.ar'],
   methods: ['POST'],
   allowedHeaders: ['Content-Type'],
 }));
